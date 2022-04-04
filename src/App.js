bd93e72f2d8f352559d3cb8858548e7c13ef25ab
Roddoark <ilyas150691@hotmail.fr>
2022-04-04 16:04:39 +0200

    React + Webpack

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = ({title}) => {
+  return (
+    <div>
+        <h1>{title}</h1>
+
+        <p>metronTest</p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+const title = 'React Test metron';
+
+ReactDOM.render(
+  <App title={title} />,
+  document.getElementById('app')
+);
