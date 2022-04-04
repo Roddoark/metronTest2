c20570ff4055acfafe1c6c87cc876a1641c71e91
Roddoark <ilyas150691@hotmail.fr>
2022-04-05 00:32:38 +0200

    Adding input with const

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App = ({ title }) => {
+  const [firstWord, setFirstWord] = useState("");
+  const [secondWord, setSecondWord] = useState("");
+
+  const [firstTweets, setFirstTweets] = useState({ tweets: [] });
+  const [secondTweets, setSecondTweets] = useState({ tweets: [] });
+
   const onClick = (e) => {
     e.preventDefault();
     console.log('okokokokokokokoko')
   }
   return (
     <div>
-      <h1 class="title">{title}</h1>
+      <h1 className="title">{title}</h1>
       <p id="sok">metronTest</p>
 
       <form>
         <div>
-          <input className='' type="text" />
-          <input className='' type="text" />
+          <input type="text" name="firstWord" id="firstWord" placeholder="React"
+            onChange={event => setFirstWord(event.target.value)}
+            value={firstWord} />
+
+          <input type="text" name="secondWord" id="secondWord" placeholder="VueJS"
+            onChange={event => setSecondWord(event.target.value)}
+            value={secondWord} />
         </div>
 
         <button onClick={onClick}>GO</button>
